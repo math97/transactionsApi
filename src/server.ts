d9d8085f164f91bf7f30d8f4863798e40df69c03
Matheus DEV <matheusdev@L-LLQPKP2GFG.lan>
2024-02-14 21:21:12 +0000

    set Ts and fastify server

---
+++ src/server.ts
@@ -0,0 +1,16 @@
+import fastify from 'fastify'
+
+
+const app = fastify()
+
+app.get('/hello', async (req,res) => {
+    res.send('hello world')
+})
+
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log('HTTP Server Running!')
+  })
