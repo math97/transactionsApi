c3d2851c8fc13180fd270bbe729a2a8957101607
Matheus DEV <matheusdev@L-LLQPKP2GFG.lan>
2024-02-15 23:05:11 +0000

    add GET transactions

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -5,11 +5,20 @@ import crypto from 'node:crypto'
 
 export async function transactionsRoutes(app: FastifyInstance) {
   app.get('/', async () => {
-    const transactions = await knex('transactions')
-      .where('amount', 1000)
-      .select('*')
+    const transactions = await knex('transactions').select()
 
-    return transactions
+    return { transactions }
+  })
+
+  app.get('/:id', async (request) => {
+    const getTransactionParamsSchema = z.object({
+      id: z.string().uuid(),
+    })
+    const { id } = getTransactionParamsSchema.parse(request.params)
+
+    const transaction = await knex('transactions').where('id', id).first()
+
+    return { transaction }
   })
 
   app.post('/', async (request, reply) => {
